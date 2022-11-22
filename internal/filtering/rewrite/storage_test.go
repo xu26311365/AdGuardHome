941538abfe517e70e469cb5b121fa13f1a3b79f3
Dimitry Kolyshev <dkolyshev@adguard.com>
2022-11-22 12:54:55 +0200

    rewrite: storage tests

--- internal/filtering/rewrite/storage_test.go
+++ internal/filtering/rewrite/storage_test.go
@@ -17,3 +17,24 @@ func TestNewDefaultStorage(t *testing.T) {
 
 	require.Equal(t, 1, len(s.List()))
 }
+
+func TestDefaultStorage_CRUD(t *testing.T) {
+	var items []*Item
+
+	s, err := NewDefaultStorage(-1, items)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(s.List()))
+
+	item := &Item{Domain: "example.com", Answer: "answer.com"}
+
+	err = s.Add(item)
+	require.NoError(t, err)
+
+	list := s.List()
+	require.Equal(t, 1, len(list))
+	require.True(t, item.equal(list[0]))
+
+	err = s.Remove(item)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(s.List()))
+}
